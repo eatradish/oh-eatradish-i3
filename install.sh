e0179450c72b5926d6ec43a5005b00225742c889
eatradish <qingbaodou@gmail.com>
2017-05-14 23:54:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cp ip.sh ~/.config/ip.sh
 cp -r konsole ~/.local/share
 cp konsolerc ~/.konsolerc
 echo "( sleep 5s ; transset-df -n i3bar 0.75 ; ) &" > ~/.xprofile
-echo "( sleep 5s ; transset-df -n xfce4-notifyd 0.75 ; ) &" >/.xprofile
+echo "( sleep 5s ; transset-df -n xfce4-notifyd 0.75 ; ) &" > ~/.xprofile
 echo "feh --bg-scale ~/.config/w.png" > ~/.xprofile
 cp w.png ~/.config/w.png
 echo "feh --bg-scale ~/.config/w.png &" > ~/.xprofile
