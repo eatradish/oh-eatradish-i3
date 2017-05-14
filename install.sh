2b53f82717a5c1967c982b8770cf87c823e3542a
eatradish <qingbaodou@gmail.com>
2017-05-14 23:55:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cp config ~/.config/i3/config
 cp .conkyrc ~/.conkyrc
 cp ip.sh ~/.config/ip.sh
 cp -r konsole ~/.local/share
-cp konsolerc ~/.konsolerc
+cp konsolerc ~/.config/konsolerc
 echo "( sleep 5s ; transset-df -n i3bar 0.75 ; ) &" > ~/.xprofile
 echo "( sleep 5s ; transset-df -n xfce4-notifyd 0.75 ; ) &" > ~/.xprofile
 echo "feh --bg-scale ~/.config/w.png" > ~/.xprofile
