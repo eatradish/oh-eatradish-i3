222e75fe1b8dc66e8d2f4ab9b25f64b345baa9d7
eatradish <qingbaodou@gmail.com>
2017-05-14 20:10:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ cp config ~/.config/i3/config
 cp .conkyrc ~/.conkyrc
 cp trans-i3bar.sh ~/.config/trans-i3bar.sh
 cp ip.sh ~/.config/ip.sh
-echo "bash trans-i3bar.sh &" > ~/.xprofile
+echo "bash ~/.config/trans-i3bar.sh &" > ~/.xprofile
 echo "feh --bg-scale ~/.config/w.png" > ~/.xprofile
 done
